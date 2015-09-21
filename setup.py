570e14c9b3f1e3a4a82c03b9563202b898508fa9
Jim Fleming <jimmy.fleming@gmail.com>
2015-09-21 11:09:32 -0700

    Add setup script for pip install

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='neural',
+  version='1.0.0',
+  url='https://github.com/BarkingMouseStudio/neural-py',
+  author='Jim Fleming',
+  author_email='jim@fomoro.com',
+  install_requires=['theano'],
+)
